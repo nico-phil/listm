0eaa06d40cc77592717242172acb69f225ab9a7d
Admin <admin@MacBook-Pro--Nicolas-Philibert.local>
2025-08-12 23:17:46 +0300

    add env variable for cassandra

---
+++ process/config/config.go
@@ -0,0 +1,25 @@
+package config
+
+import (
+	"os"
+	"strings"
+)
+
+func GetContactPoints() []string {
+	valueStr := os.Getenv("CASSANDRA_CONTACT_POINTS")
+	if valueStr == "" {
+		return []string{"cassandra-node1:9042"}
+	}
+
+	valueArray := strings.Split(valueStr, "")
+	return valueArray
+}
+
+func GetKeyspace() string {
+	keypsace := os.Getenv("CASSANDRA_KEYSPACE")
+	if keypsace == "" {
+		return "listm"
+	}
+
+	return keypsace
+}
