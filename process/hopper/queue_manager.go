a7e342f3812d7e504f5fc9ab63aec163587bd71a
Admin <admin@MacBook-Pro--Admin.local>
2025-08-27 19:08:21 +0300

    start module hopper

--- process/hopper/queue_manager.go
+++ process/hopper/queue_manager.go
@@ -1,9 +1,108 @@
 package hopper
 
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/nico-phil/process/db"
+)
+
 type QueueManager struct {
 }
 
 func NewQueueManager() *QueueManager {
-
 	return &QueueManager{}
+}
+func (qm *QueueManager) ProcessAllWorkspacesWithContext(ctx context.Context) error {
+	// get capaigns or each worksapce
+	// get active list for each compaign
+	// get leads for each list
+	campaigns, err := db.GetCampaigns()
+	if err != nil {
+		log.Printf("failed to get campaign from db %v", err)
+		return err
+	}
+
+	workspaces := map[string][]db.Campaign{}
+
+	for _, c := range campaigns {
+		if c.Active {
+			workspaces[c.WorkspaceID] = append(workspaces[c.WorkspaceID], c)
+		}
+	}
+
+	totalWorkspaces := len(workspaces)
+	processedWorkspaces := 0
+
+	for workspaceID, campaign := range workspaces {
+		err := qm.ProcessWorkspaceWithContext(ctx, workspaceID, campaign)
+		if err != nil {
+			log.Printf("failed to process workspace %s: %v", workspaceID, err)
+			continue
+		}
+
+		log.Printf("successfully process workspace %s", workspaceID)
+
+		processedWorkspaces++
+	}
+	log.Printf("processed %d/%d workspaces succesfully", processedWorkspaces, totalWorkspaces)
+	return nil
+}
+
+func (qm *QueueManager) ProcessWorkspaceWithContext(ctx context.Context, worksapceID string, campaigns []db.Campaign) error {
+
+	activeCampgaignWithSchedule := qm.GetActiveCampignsWithSchedule(worksapceID, campaigns)
+	fmt.Printf("activeCampgaignWithSchedule for %s %v\n", worksapceID, activeCampgaignWithSchedule)
+
+	if len(activeCampgaignWithSchedule) == 0 {
+		log.Printf("no active campaign found for workspace %s", worksapceID)
+	}
+
+	log.Printf("found %d active campaigns for %s", len(activeCampgaignWithSchedule), worksapceID)
+
+	for _, campaign := range activeCampgaignWithSchedule {
+		// process the campaign for the worskspace
+		injected, err := qm.ProcessCampaignWithContext(ctx, campaign)
+		if err != nil {
+
+		}
+		fmt.Println(injected)
+
+	}
+	return nil
+}
+
+func (qm *QueueManager) GetActiveCampignsWithSchedule(worksapceID string, campaigns []db.Campaign) []db.Campaign {
+
+	campaignsWithSchedule := []db.Campaign{}
+
+	for _, campaign := range campaigns {
+		currentTime := time.Now()
+		currentHour := currentTime.Hour()
+		currentWeekDay := currentTime.Weekday()
+
+		if currentHour >= campaign.DialStartHour && currentHour <= campaign.DialEndHour && contains(currentWeekDay, campaign.DialDays) {
+			campaignsWithSchedule = append(campaignsWithSchedule, campaign)
+		}
+	}
+
+	return campaignsWithSchedule
+}
+
+func (qm *QueueManager) ProcessCampaignWithContext(ctx context.Context, campaign db.Campaign) (int, error) {
+	log.Printf("processing campaign %s", campaign.ID)
+
+	return 0, nil
+}
+
+func contains(currentWeekDay time.Weekday, days []int) bool {
+	for _, day := range days {
+		if int(currentWeekDay) == day {
+			return true
+		}
+	}
+
+	return false
 }
--- process/orchestrator/scheduler.go
+++ process/orchestrator/scheduler.go
@@ -2,14 +2,14 @@ package orchestrator
 
 import (
 	"context"
-	"fmt"
 	"log"
-	"time"
 
-	"github.com/nico-phil/process/db"
+	"github.com/nico-phil/process/hopper"
 )
 
-type Orchestrator struct{}
+type Orchestrator struct {
+	queueManager *hopper.QueueManager
+}
 
 func New() *Orchestrator {
 	return &Orchestrator{}
@@ -17,98 +17,6 @@ func New() *Orchestrator {
 
 func (o *Orchestrator) Start() {
 	log.Printf("starting orchestrator")
-	processAllWorkspacesWithContext(context.Background())
+	o.queueManager.ProcessAllWorkspacesWithContext(context.Background())
 	// this function will get 5 min of data in the database and put it in redis
-}
-
-func processAllWorkspacesWithContext(ctx context.Context) error {
-	// get capaigns or each worksapce
-	// get active list for each compaign
-	// get leads for each list
-	campaigns, err := db.GetCampaigns()
-	if err != nil {
-		log.Printf("failed to get campaign from db %v", err)
-		return err
-	}
-
-	workspaces := map[string][]db.Campaign{}
-
-	for _, c := range campaigns {
-		if c.Active {
-			workspaces[c.WorkspaceID] = append(workspaces[c.WorkspaceID], c)
-		}
-	}
-
-	totalWorkspaces := len(workspaces)
-	processedWorkspaces := 0
-
-	for workspaceID, campaign := range workspaces {
-		err := processWorkspaceWithContext(ctx, workspaceID, campaign)
-		if err != nil {
-			log.Printf("failed to process workspace %s: %v", workspaceID, err)
-			continue
-		}
-
-		log.Printf("successfully process workspace %s", workspaceID)
-
-		processedWorkspaces++
-	}
-	log.Printf("processed %d/%d workspaces succesfully", processedWorkspaces, totalWorkspaces)
-	return nil
-}
-
-func processWorkspaceWithContext(ctx context.Context, worksapceID string, campaigns []db.Campaign) error {
-
-	activeCampgaignWithSchedule := getActiveCampignsWithSchedule(worksapceID, campaigns)
-	fmt.Printf("activeCampgaignWithSchedule for %s %v\n", worksapceID, activeCampgaignWithSchedule)
-
-	if len(activeCampgaignWithSchedule) == 0 {
-		log.Printf("no active campaign found for workspace %s", worksapceID)
-	}
-
-	log.Printf("found %d active campaigns for %s", len(activeCampgaignWithSchedule), worksapceID)
-
-	for _, campaign := range activeCampgaignWithSchedule {
-		// process the campaign for the worskspace
-		injected, err := processCampaignWithContext(ctx, campaign)
-		if err != nil {
-
-		}
-		fmt.Println(injected)
-
-	}
-	return nil
-}
-
-func getActiveCampignsWithSchedule(worksapceID string, campaigns []db.Campaign) []db.Campaign {
-
-	campaignsWithSchedule := []db.Campaign{}
-
-	for _, campaign := range campaigns {
-		currentTime := time.Now()
-		currentHour := currentTime.Hour()
-		currentWeekDay := currentTime.Weekday()
-
-		if currentHour >= campaign.DialStartHour && currentHour <= campaign.DialEndHour && contains(currentWeekDay, campaign.DialDays) {
-			campaignsWithSchedule = append(campaignsWithSchedule, campaign)
-		}
-	}
-
-	return campaignsWithSchedule
-}
-
-func processCampaignWithContext(ctx context.Context, campaign db.Campaign) (int, error) {
-	log.Printf("processing campaign %s", campaign.ID)
-
-	return 0, nil
-}
-
-func contains(currentWeekDay time.Weekday, days []int) bool {
-	for _, day := range days {
-		if int(currentWeekDay) == day {
-			return true
-		}
-	}
-
-	return false
 }
