9afea245f8ce80a03fc80f93c45daf77d058bbde
Admin <admin@MacBook-Pro--Admin.local>
2025-09-04 18:25:13 +0300

    test cache for concurrency

--- process/tz/zipcode_test.go
+++ process/tz/zipcode_test.go
@@ -1,7 +1,10 @@
 package tz
 
 import (
+	"fmt"
+	"sync"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -11,6 +14,7 @@ func TestNewZipcodeCache(t *testing.T) {
 
 	assert.NotNil(t, zipCodeCache)
 	assert.NotNil(t, zipCodeCache.cache)
+	assert.Len(t, zipCodeCache.cache, 0)
 }
 
 // TestSetZipCode tests the Set zipcode function
@@ -41,5 +45,101 @@ func TestSetZipCode(t *testing.T) {
 
 	_, ok = zipCodeCache.cache["4444"]
 	assert.False(t, ok)
+}
+
+func TestZipCodeCacheConcurrency(t *testing.T) {
+	cache := NewZipcodeCache()
+	var wg sync.WaitGroup
+
+	for i := 0; i < 100; i++ {
+		wg.Add(2)
+
+		go func() {
+			defer wg.Done()
+			info := ZipCodeInfo{
+				Zipcode:  fmt.Sprintf("zip%d", i),
+				TimeZone: "America/New_York",
+			}
+
+			cache.Set(info.Zipcode, &info)
+		}()
+
+		go func() {
+			defer wg.Done()
+			zipCode := fmt.Sprintf("zip%d", i)
+			_, _ = cache.getZipcode(zipCode)
+
+		}()
+	}
+
+	wg.Wait()
+
+	assert.Len(t, cache.cache, 100)
+}
+
+func TestGetLocalTimeAt(t *testing.T) {
+
+	zipCodeCache := NewZipcodeCache()
+
+	// Eastern time zone
+	info1 := ZipCodeInfo{
+		Zipcode:   "1234",
+		Latitude:  40.7128,
+		Longitude: -74.0060,
+		City:      "New York",
+		State:     "New York",
+		TimeZone:  "America/New_York",
+	}
+
+	info2 := ZipCodeInfo{
+		Zipcode:   "94016",
+		Latitude:  37.7749,
+		Longitude: -122.4194,
+		City:      "San Francisco",
+		State:     "California",
+		TimeZone:  "America/Los_Angeles",
+	}
+
+	zipCodeCache.Set(info1.Zipcode, &info1)
+	zipCodeCache.Set(info2.Zipcode, &info2)
+
+	now := time.Now()
+
+	_, err := GetLocalTimeAt(zipCodeCache, info1.Zipcode, now)
+	if err != nil {
+		t.Fatalf("failed to get local time for zipcode 1234")
+	}
+
+	_, err = GetLocalTimeAt(zipCodeCache, info2.Zipcode, now)
+	if err != nil {
+		t.Fatalf("failed to get local time 94016")
+	}
+
+	// fmt.Printf("time1: %v\n", r1)
+	// fmt.Printf("time2: %v\n", r2)
+
+	easternLoc, _ := time.LoadLocation("America/New_York")
+	pacificLoc, _ := time.LoadLocation("America/Los_Angeles")
+
+	inEastern := now.In(easternLoc)
+	inPacific := now.In(pacificLoc)
+
+	hourDiff := inEastern.Hour() - inPacific.Hour()
+
+	if hourDiff < 0 {
+		hourDiff += 24
+	} else if hourDiff > 3 {
+		hourDiff = 3
+	}
+
+	if hourDiff != 3 {
+		t.Errorf("Expected 3 hour difference between Eastern and Pacific, got %d", hourDiff)
+	}
+
+	// Test non-existent zip code
+	_, err = GetLocalTimeAt(zipCodeCache, "99999", now)
+	if err == nil {
+		t.Errorf("Expected error for non-existent zip code 99999, got nil")
+	}
 
 }
